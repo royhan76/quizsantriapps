6df27145f23c73b3910364ee69775d103525837d
beensholih76 <beensholih@gmail.com>
2020-10-14 08:04:30 +0700

    push101420

---
+++ lib/Home.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quizapps/quizpage.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> images = [
+    "images/py.png",
+    "images/java.png",
+    "images/js.png",
+    "images/cpp.png",
+    "images/linux.png",
+  ];
+
+  List<String> des = [
+    "Python is one of the most popular and fastest programming language since half a decade.\nIf You think you have learnt it.. \nJust test yourself !!",
+    "Java has always been one of the best choices for Enterprise World. If you think you have learn the Language...\nJust Test Yourself !!",
+    "Javascript is one of the most Popular programming language supporting the Web.\nIt has a wide range of Libraries making it Very Powerful !",
+    "C++, being a statically typed programming language is very powerful and Fast.\nit's DMA feature makes it more useful. !",
+    "Linux is a OPEN SOURCE Operating System which powers many Servers and Workstation.\nIt is also a top Priority in Developement Work !",
+  ];
+
+  Widget customCard(String langname, String image, String des) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 30.0,
+      ),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => getjson(langname),
+          ));
+        },
+        child: Material(
+          color: Colors.indigoAccent,
+          elevation: 10.0,
+          borderRadius: BorderRadius.circular(25.0),
+          child: Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                  ),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(100.0),
+                    child: Container(
+                      // changing from 200 to 150 as to look better
+                      height: 150.0,
+                      width: 150.0,
+                      child: ClipOval(
+                        child: Image(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            image,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    langname,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontFamily: "Quando",
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    des,
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                        fontFamily: "Alike"),
+                    maxLines: 5,
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Latihan Soal",
+          style: TextStyle(
+            fontFamily: "Quando",
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          customCard("Nahwu", images[1], des[1]),
+        ],
+      ),
+    );
+  }
+}
